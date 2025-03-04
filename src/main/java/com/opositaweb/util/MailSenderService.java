88c1dda042effa4591b2cf69be468e84cd5db6cc
Jebimeta <jebilongo@gmail.com>
2025-03-04 18:15:12 +0100

    Se añade metodo sendVerificationEmail y sendPasswordRecoveryEmail

--- src/main/java/com/opositaweb/util/MailSenderService.java
+++ src/main/java/com/opositaweb/util/MailSenderService.java
@@ -4,12 +4,13 @@ import com.opositaweb.config.properties.OpositaWebProperties;
 import com.opositaweb.domain.vo.EmailResponse;
 import com.opositaweb.exception.AppErrorCode;
 import com.opositaweb.exception.BusinessException;
+import com.opositaweb.repository.entities.Customer;
 import com.opositaweb.service.about.EmailResponseFactory;
 import com.opositaweb.service.user.CustomerService;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -18,19 +19,25 @@ import java.io.UnsupportedEncodingException;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class MailSenderService {
 
     private final JavaMailSender mailSender;
+
     private final OpositaWebProperties properties;
+
     private final CustomerService customerService;
 
+    private final CustomerService tokenService;
+
+    // Envia un correo electrónico al administrador con los datos y el mensaje del remitente.
     public EmailResponse receiveContactUs(String senderName, String phoneNumber, String emailMessage)
-        throws BusinessException {
+            throws BusinessException {
 
         String subject = "Contacta con nosotros";
         String email = properties.getMail().getHostEmail();
         String headerMessage = "Se ha recibido un correo a través del formulario de contacta con nosotros, "
-                 + "los datos del remitente son los siguientes: ";
+                + "los datos del remitente son los siguientes: ";
         String content = headerMessage + "\n"
                 + "Nombre: " + senderName + "\n"
                 + "Teléfono: " + phoneNumber + "\n"
@@ -44,8 +51,9 @@ public class MailSenderService {
         }
     }
 
+    // Envia el correo electrónico
     private void sendMessage(String email, String senderName, String subject, String content)
-        throws MessagingException, UnsupportedEncodingException {
+            throws MessagingException, UnsupportedEncodingException {
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
@@ -55,6 +63,55 @@ public class MailSenderService {
         helper.setText(content, true);
 
         mailSender.send(message);
+    }
+
+    // Envia un correo de verificación al usuario
+    public void sendVerificationEmail(Customer user) throws BusinessException {
+        String subject = "Verificación de Registro";
+        String senderName = "Rita Rouge";
+        String verifyURL = properties.getMail().getHost() + "/auth/verify/" + user.getVerificationToken();
+
+        String content = "Estimado " + user.getName() + ",<br>"
+                + "Por favor, haga clic en el siguiente enlace para verificar su registro:<br>" + "<h3><a href=\""
+                + verifyURL + "\" target=\"_self\">VERIFICAR</a></h3>" + "Gracias,<br>" + senderName + ".";
+
+        try {
+            sendMessage(user.getEmail(), senderName, subject, content);
+        }
+        catch (MessagingException | UnsupportedEncodingException e) {
+            log.error(e);
+            throw new BusinessException(AppErrorCode.ERROR_EMAIL);
+        }
+
+    }
+
+    // Envia un correo de recuperación de contraseña
+    public EmailResponse sendPasswordRecoveryEmail(String email) {
+        String subject = "Restablecer contraseña";
+        String senderName = "OpositaWeb";
+
+        Customer customer = customerService.findByEmail(email);
+
+        String verificationToken = tokenService.generateVerificationToken(customer);
+        customer.setVerificationToken(verificationToken);
+
+        String verifyURL = properties.getMail().getHost() + "/auth/password-reset/request/"
+                + customer.getVerificationToken();
+
+        String content = "Estimado " + customer.getName() + ",<br>"
+                + "Por favor, haga clic en el siguiente enlace para asignar una nueva contraseña:<br>"
+                + "<h3><a href=\"" + verifyURL + "\" target=\"_self\">CAMBIAR CONTRASEÑA</a></h3>" + "Gracias,<br>"
+                + senderName + ".";
+
+        try {
+            sendMessage(customer.getEmail(), senderName, subject, content);
+            customerService.update(customer);
+        }
+        catch (MessagingException | UnsupportedEncodingException e) {
+            log.error(e);
+            throw new BusinessException(AppErrorCode.ERROR_EMAIL);
+        }
+        return new EmailResponse("Se ha creado el correo correctamente.");
     }
 
 }
