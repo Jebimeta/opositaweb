14a12bd13f8dc1632694b9e8001c93fe2d3bc2d9
Jebimeta <jebilongo@gmail.com>
2025-03-08 18:00:18 +0100

    Modificacion de los cors

--- src/main/java/com/opositaweb/config/WebConfig.java
+++ src/main/java/com/opositaweb/config/WebConfig.java
@@ -8,17 +8,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
+    // Define un bean que configura CORS para la aplicación.
     @Bean
-    public WebMvcConfigurer webMvcConfigurer() {
+    public WebMvcConfigurer corsConfigurer(){
         return new WebMvcConfigurer() {
+            // Sobrescribe el método para agregar configuraciones de CORS.
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(false)
-                        .maxAge(3600);
+            public void addCorsMappings(CorsRegistry registry){
+                // Configura CORS para permitir todas las solicitudes desde cualquier origen, con cualquier método y encabezado.
+                registry.addMapping("/**") // Permite CORS para todas las rutas.
+                        .allowedOrigins("*") // Permite solicitudes desde cualquier origen.
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Permite todos los métodos HTTP (GET, POST, etc.).
+                        .allowedHeaders("*") // Permite todos los encabezados HTTP.
+                        .allowCredentials(true) // No permite el envío de credenciales (cookies, cabeceras de autenticación, etc.).
+                        .maxAge(3600); // Establece el tiempo máximo (en segundos) que los resultados de una solicitud preflight pueden ser almacenados en caché. // Establece el tiempo máximo en segundos que los resultados de una solicitud preflight pueden ser almacenados en caché.
             }
         };
     }
